1c3a5d605ff3fb923e12164cd6160012379d87bf
Daniel Stolarski <d.stolarski@o2.pl>
2023-01-02 19:41:48 +0100

    Tile class finished

--- src/main/java/Tile.java
+++ src/main/java/Tile.java
@@ -1,2 +1,28 @@
-public class Tile {
+public abstract class Tile {
+
+    private int tileCoordinate;
+
+    public Tile(int tileCoordinate){
+        this.tileCoordinate = tileCoordinate;
+    }
+
+    public abstract boolean isTileOccupied();
+
+    public abstract Piece getPiece();
+
+    public static final class EmptyTile extends Tile{
+        public EmptyTile(int coordinate) {
+            super(coordinate);
+        }
+
+        @Override
+        public boolean isTileOccupied() {
+            return false;
+        }
+
+        @Override
+        public Piece getPiece() {
+            return null;
+        }
+    }
 }
