2040f4180648c961e14cc6003eeea11ec47360ef
Daniel Stolarski <d.stolarski@o2.pl>
2023-01-03 12:14:41 +0100

    Tile class updated

--- src/main/java/Tile.java
+++ src/main/java/Tile.java
@@ -25,4 +25,24 @@ public abstract class Tile {
             return null;
         }
     }
+
+    public static final class OccupiedTile extends Tile {
+
+        private Piece pieceOnTile;
+
+        public OccupiedTile(int tileCoordinate, Piece pieceOnTile) {
+            super(tileCoordinate);
+            this.pieceOnTile = pieceOnTile;
+        }
+
+        @Override
+        public boolean isTileOccupied() {
+            return true;
+        }
+
+        @Override
+        public Piece getPiece() {
+            return this.pieceOnTile;
+        }
+    }
 }
