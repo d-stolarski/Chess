7e04e6f007a1893d6bae56b6b46ef5dbcc68484b
Daniel Stolarski <d.stolarski@o2.pl>
2023-01-09 09:24:56 +0100

    feat: add logic to Pawn class

--- src/main/java/com/daniel/chess/engine/pieces/Pawn.java
+++ src/main/java/com/daniel/chess/engine/pieces/Pawn.java
@@ -9,6 +9,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import static com.daniel.chess.engine.board.Move.*;
+
 public class Pawn extends Piece{
 
     private final static int[] CANDIDATE_MOVE_VECTOR_COORDINATES = {8, 16};
@@ -30,15 +32,17 @@ public class Pawn extends Piece{
             }
 
             if(currentCandidateOffset == 8 && board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
-                legalMoves.add(new Move.MajorMove(board, this, candidateDestinationCoordinate));
+                legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
             } else if(currentCandidateOffset == 16 && this.isFirstMove() &&
                     (BoardUtils.SECOND_ROW[this.piecePosition] && this.getPieceAlliance().isBlack()) ||
                     (BoardUtils.SEVENTH_ROW[this.piecePosition] && this.getPieceAlliance().isWhite())) {
-
-
+                final int behindCandidateDestinationCoordinate = this.piecePosition + (this.pieceAlliance.getDirection() * 8);
+                if(!board.getTile(behindCandidateDestinationCoordinate).isTileOccupied() &&
+                        !board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
+                    legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+                }
             }
         }
-
         return legalMoves;
     }
 }
