9e728e171043f540538f57fd3837c274a22d3537
Daniel Stolarski <d.stolarski@o2.pl>
2023-01-10 11:25:04 +0100

    feat: add MoveTransition class

--- src/main/java/com/daniel/chess/engine/player/MoveTransition.java
+++ src/main/java/com/daniel/chess/engine/player/MoveTransition.java
@@ -1,4 +1,19 @@
 package com.daniel.chess.engine.player;
 
+import com.daniel.chess.engine.board.Board;
+import com.daniel.chess.engine.board.Move;
+
 public class MoveTransition {
+
+    private final Board transitionBoard;
+    private final Move move;
+    private final MoveStatus moveStatus;
+
+    public MoveTransition(final Board transitionBoard,
+                          final Move move,
+                          final MoveStatus moveStatus) {
+        this.transitionBoard = transitionBoard;
+        this.move = move;
+        this.moveStatus = moveStatus;
+    }
 }
