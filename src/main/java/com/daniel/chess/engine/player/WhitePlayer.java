be361a8377384de0f17c088eed2ec531e8e16ac6
Daniel Stolarski <d.stolarski@o2.pl>
2023-01-10 10:21:44 +0100

    feat: add super() constructors to WhitePlayer and BlackPlayer class

--- src/main/java/com/daniel/chess/engine/player/WhitePlayer.java
+++ src/main/java/com/daniel/chess/engine/player/WhitePlayer.java
@@ -2,13 +2,20 @@ package com.daniel.chess.engine.player;
 
 import com.daniel.chess.engine.board.Board;
 import com.daniel.chess.engine.board.Move;
+import com.daniel.chess.engine.pieces.Piece;
 
 import java.util.Collection;
 
-public class WhitePlayer {
+public class WhitePlayer extends Player{
 
     public WhitePlayer(Board board,
                        Collection<Move> whiteStandardLegalMoves,
                        Collection<Move> blackStandardLegalMoves) {
+        super(board, whiteStandardLegalMoves, blackStandardLegalMoves);
+    }
+
+    @Override
+    public Collection<Piece> getActivePieces() {
+        return this.board.getWhitePieces();
     }
 }
