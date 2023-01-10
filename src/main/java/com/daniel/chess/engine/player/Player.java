18f90a8fc5e6ff91bac844e172ef61eba3a77713
Daniel Stolarski <d.stolarski@o2.pl>
2023-01-10 13:17:07 +0100

    feat: add imp to method in Player class

--- src/main/java/com/daniel/chess/engine/player/Player.java
+++ src/main/java/com/daniel/chess/engine/player/Player.java
@@ -92,7 +92,10 @@ public abstract class Player {
         final Board transitionBoard = move.execute();
         final Collection<Move> kingAttacks = Player.calculateAttacksOnTile(transitionBoard.currentPlayer().
                 getOpponent().getPlayerKing().getPiecePosition(), transitionBoard.currentPlayer().getLegalMoves());
-        return null;
+        if(!kingAttacks.isEmpty()) {
+            return new MoveTransition(this.board, move, MoveStatus.LEAVES_PLAYER_IN_CHECK);
+        }
+        return new MoveTransition(transitionBoard, move, MoveStatus.DONE);
     }
 
     public abstract Collection<Piece> getActivePieces();
