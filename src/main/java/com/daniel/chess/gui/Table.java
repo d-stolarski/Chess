871baf053053758c182d000f7c9289ea2b90184c
Daniel Stolarski <d.stolarski@o2.pl>
2023-01-13 10:42:59 +0100

    feat: add methods to Table GUI class

--- src/main/java/com/daniel/chess/gui/Table.java
+++ src/main/java/com/daniel/chess/gui/Table.java
@@ -2,6 +2,8 @@ package com.daniel.chess.gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Table {
 
@@ -10,7 +12,24 @@ public class Table {
 
     public Table() {
         this.gameFrame = new JFrame("JChess");
+        final JMenuBar tableMenuBar = new JMenuBar();
+        populateMenuBar(tableMenuBar);
         this.gameFrame.setSize(OUTER_FRAME_DIMENSION);
         this.gameFrame.setVisible(true);
+    }
+
+    private void populateMenuBar(final JMenuBar tableMenuBar) {
+        tableMenuBar.add(createFileMenu());
+    }
+
+    private JMenu createFileMenu() {
+        final JMenu fileMenu = new JMenu("File");
+        final JMenuItem openPGN = new JMenuItem("Load PGN File");
+        openPGN.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("open up that pgn file");
+            }
+        });
     }
 }
