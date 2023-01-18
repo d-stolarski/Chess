9ba95aac48f43c600e0869983cceb70e9d202caf
Daniel Stolarski <d.stolarski@o2.pl>
2023-01-18 10:09:22 +0100

    feat: add logic to GameHistoryPanel class

--- src/main/java/com/daniel/chess/gui/GameHistoryPanel.java
+++ src/main/java/com/daniel/chess/gui/GameHistoryPanel.java
@@ -1,4 +1,32 @@
 package com.daniel.chess.gui;
 
-public class GameHistoryPanel {
+import com.daniel.chess.engine.board.Board;
+
+import javax.swing.*;
+import java.awt.*;
+
+import static com.daniel.chess.gui.Table.*;
+
+public class GameHistoryPanel extends JPanel {
+
+    private final DataModel model;
+    private final JScrollPane scrollPane;
+    private static final Dimension HISTORY_PANEL_DIMENSION = new Dimension(100, 400);
+
+    GameHistoryPanel() {
+        this.setLayout(new BorderLayout());
+        this.model = new DataModel();
+        final JTable table = new JTable(model);
+        table.setRowHeight(15);
+        this.scrollPane = new JScrollPane(table);
+        scrollPane.setColumnHeaderView(table.getTableHeader());
+        scrollPane.setPreferredSize(HISTORY_PANEL_DIMENSION);
+        this.add(scrollPane, BorderLayout.CENTER);
+        this.setVisible(true);
+    }
+
+    void redo(final Board board, final MoveLog moveLog)  {
+
+    }
+
 }
