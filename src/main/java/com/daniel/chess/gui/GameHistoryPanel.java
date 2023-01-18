9c0784dfb01134c54c46e664f5da83e3f7ac4306
Daniel Stolarski <d.stolarski@o2.pl>
2023-01-18 11:18:35 +0100

    feat: add logic to GameHistryClass

--- src/main/java/com/daniel/chess/gui/GameHistoryPanel.java
+++ src/main/java/com/daniel/chess/gui/GameHistoryPanel.java
@@ -1,6 +1,7 @@
 package com.daniel.chess.gui;
 
 import com.daniel.chess.engine.board.Board;
+import com.daniel.chess.engine.board.Move;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
@@ -31,7 +32,32 @@ public class GameHistoryPanel extends JPanel {
     }
 
     void redo(final Board board,
-              final MoveLog moveLog)  {
+              final MoveLog moveHistory)  {
+
+        int currentRow = 0;
+        this.model.clear();
+        for(final Move move : moveHistory.getMoves()) {
+            final String moveText = move.toString();
+            if(move.getMovedPiece().getPieceAlliance().isWhite()) {
+                this.model.setValueAt(moveText, currentRow, 0);
+            } else if(move.getMovedPiece().getPieceAlliance().isBlack()) {
+                this.model.setValueAt(moveText, currentRow, 1);
+                currentRow++;
+            }
+        }
+
+        if(moveHistory.getMoves().size() > 0) {
+            final Move lastMove = moveHistory.getMoves().get(moveHistory.size() - 1);
+            final String moveText = lastMove.toString();
+
+            if(lastMove.getMovedPiece().getPieceAlliance().isWhite()) {
+                this.model.setValueAt(moveText + calculateCheckAndCheckMateHash(board), currentRow - 1, 1);
+            }
+        }
+
+        final JScrollBar vertical = scrollPane.getVerticalScrollBar();
+        vertical.setValue(vertical.getMaximum());
+
 
     }
 
@@ -91,6 +117,17 @@ public class GameHistoryPanel extends JPanel {
                 fireTableCellUpdated(row, column);
             }
         }
+
+        @Override
+        public Class<?> getColumnClass(final int column) {
+            return Move.class;
+        }
+
+        @Override
+        public String getColumnName(final int column) {
+            return NAMES[column];
+        }
+
     }
 
     private static class Row {
