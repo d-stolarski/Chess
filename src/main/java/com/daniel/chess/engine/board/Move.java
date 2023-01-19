9695b06d9b1d1e768fb2172822ff15a6a3e9f084
Daniel Stolarski <d.stolarski@o2.pl>
2023-01-19 10:35:24 +0100

    feat: add logic to Move class

--- src/main/java/com/daniel/chess/engine/board/Move.java
+++ src/main/java/com/daniel/chess/engine/board/Move.java
@@ -97,6 +97,26 @@ public abstract class Move {
         return builder.build();
     }
 
+    public static class MajorAttackMove extends AttackMove {
+
+        public MajorAttackMove(final Board board,
+                               final Piece pieceMoved,
+                               final int destinationCoordinate,
+                               final Piece pieceAttacked) {
+            super(board, pieceMoved, destinationCoordinate, pieceAttacked);
+        }
+
+        @Override
+        public boolean equals(final Object other) {
+            return this == other || other instanceof MajorAttackMove && super.equals(other);
+        }
+
+        @Override
+        public String toString() {
+            return movedPiece.getPieceType() + BoardUtils.getPositionAtCoordinate(this.destinationCoordinate);
+        }
+    }
+
     public static class MajorMove extends Move {
 
         public MajorMove(final Board board,
