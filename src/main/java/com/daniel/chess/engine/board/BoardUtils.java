75824fcf07afeedfc39fb11d4eb26f7e47fce46c
Daniel Stolarski <d.stolarski@o2.pl>
2023-01-30 09:01:58 +0100

    add logic to MoveTransition class and BoardUtils class.

--- src/main/java/com/daniel/chess/engine/board/BoardUtils.java
+++ src/main/java/com/daniel/chess/engine/board/BoardUtils.java
@@ -1,10 +1,11 @@
 package com.daniel.chess.engine.board;
 
+import com.daniel.chess.engine.pieces.King;
+import com.daniel.chess.engine.pieces.Piece;
+import com.daniel.chess.engine.player.MoveTransition;
 import com.google.common.collect.ImmutableMap;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public enum BoardUtils {
 
@@ -28,19 +29,47 @@ public enum BoardUtils {
     public final List<Boolean> SEVENTH_ROW = initRow(48);
     public final List<Boolean> EIGHTH_ROW = initRow(56);
 
-    public static final String[] ALGEBRAIC_NOTATION = initializeAlgebraicNotation();
-    public static final Map<String, Integer> POSITION_TO_COORDINATE = initializePositionToCoordinateMap();
+    public final List<String> ALGEBRAIC_NOTATION = initializeAlgebraicNotation();
+    public final Map<String, Integer> POSITION_TO_COORDINATE = initializePositionToCoordinateMap();
 
-
+    public static final int START_TILE_INDEX = 0;
     public static final int NUM_TILES = 64;
     public static final int NUM_TILES_PER_ROW = 8;
 
-    private BoardUtils() {
-        throw new RuntimeException("You cannot instantiate!");
+    private static List<Boolean> initColumn(int columnNumber) {
+        final Boolean[] column = new Boolean[NUM_TILES];
+        for(int i = 0; i < column.length; i++) {
+            column[i] = false;
+        }
+        do {
+            column[columnNumber] = true;
+            columnNumber += NUM_TILES_PER_ROW;
+        } while(columnNumber < NUM_TILES);
+        return Collections.unmodifiableList(Arrays.asList(column));
     }
 
-    private static String[] initializeAlgebraicNotation() {
-        return new String[] {
+    private static List<Boolean> initRow(int rowNumber) {
+        final Boolean[] row = new Boolean[NUM_TILES];
+        for(int i = 0; i < row.length; i++) {
+            row[i] = false;
+        }
+        do {
+            row[rowNumber] = true;
+            rowNumber++;
+        } while(rowNumber % NUM_TILES_PER_ROW != 0);
+        return Collections.unmodifiableList(Arrays.asList(row));
+    }
+
+    private Map<String, Integer> initializePositionToCoordinateMap() {
+        final Map<String, Integer> positionToCoordinate = new HashMap<>();
+        for(int i = START_TILE_INDEX; i < NUM_TILES; i++) {
+            positionToCoordinate.put(ALGEBRAIC_NOTATION.get(i), i);
+        }
+        return Collections.unmodifiableMap(positionToCoordinate);
+    }
+
+    private static List<String> initializeAlgebraicNotation() {
+        return Collections.unmodifiableList(Arrays.asList(
                 "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
                 "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
                 "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
@@ -48,45 +77,41 @@ public enum BoardUtils {
                 "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
                 "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
                 "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
-                "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"
-        };
-    }
-
-    private static Map<String, Integer> initializePositionToCoordinateMap() {
-        final Map<String, Integer> positionToCoordinate = new HashMap<>();
-        for(int i = 0; i < NUM_TILES; i++) {
-            positionToCoordinate.put(ALGEBREIC_NOTATION[i], i);
-        }
-        return ImmutableMap.copyOf(positionToCoordinate);
-    }
-
-    private static List<Boolean> initColumn(int columnNumber) {
-        final boolean[] column = new boolean[NUM_TILES];
-        do {
-            column[columnNumber] = true;
-            columnNumber += NUM_TILES_PER_ROW;
-        } while(columnNumber < NUM_TILES);
-        return column;
-    }
-
-    private static List<Boolean> initRow(int rowNumber) {
-        final boolean[] row = new boolean[NUM_TILES];
-        do {
-            row[rowNumber] = true;
-            rowNumber++;
-        } while(rowNumber % NUM_TILES_PER_ROW != 0);
-        return row;
+                "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"));
     }
 
     public static boolean isValidTileCoordinate(final int coordinate) {
-        return coordinate >= 0 && coordinate < NUM_TILES;
+        return coordinate >= START_TILE_INDEX && coordinate < NUM_TILES;
     }
 
-    public static int getCoordinateAtPosition(final String position) {
+    public int getCoordinateAtPosition(final String position) {
         return POSITION_TO_COORDINATE.get(position);
-   }
+    }
 
-   public static String getPositionAtCoordinate(final int coordinate) {
-       return ALGEBREIC_NOTATION[coordinate];
-   }
+    public String getPositionAtCoordinate(final int coordinate) {
+        return ALGEBRAIC_NOTATION.get(coordinate);
+    }
+
+    public static boolean isThreatenedBoardImmediate(final Board board) {
+        return board.whitePlayer().isInCheck() || board.blackPlayer().isInCheck();
+    }
+
+    public static boolean kingThreat(final Move move) {
+        final Board board = move.getBoard();
+        final MoveTransition transition = board.currentPlayer().makeMove(move);
+        return transition.getToBoard().currentPlayer().isInCheck();
+    }
+
+    public static boolean isKingPawnTrap(final Board board,
+                                         final King king,
+                                         final int frontTile) {
+        final Piece piece = board.getPiece(frontTile);
+        return piece != null && piece.getPieceType() == Piece.PieceType.PAWN &&
+               piece.getPieceAlliance() != king.getPieceAlliance();
+    }
+
+
+
+
+
 }
--- src/main/java/com/daniel/chess/engine/player/MoveTransition.java
+++ src/main/java/com/daniel/chess/engine/player/MoveTransition.java
@@ -3,27 +3,36 @@ package com.daniel.chess.engine.player;
 import com.daniel.chess.engine.board.Board;
 import com.daniel.chess.engine.board.Move;
 
-public class MoveTransition {
+public final class MoveTransition {
 
-    private final Board transitionBoard;
-    private final Move move;
+    private final Board fromBoard;
+    private final Board toBoard;
+    private final Move transitionMove;
     private final MoveStatus moveStatus;
 
-    public MoveTransition(final Board transitionBoard,
-                          final Move move,
+    public MoveTransition(final Board fromBoard,
+                          final Board toBoard,
+                          final Move transitionMove,
                           final MoveStatus moveStatus) {
-        this.transitionBoard = transitionBoard;
-        this.move = move;
+        this.fromBoard = fromBoard;
+        this.toBoard = toBoard;
+        this.transitionMove = transitionMove;
         this.moveStatus = moveStatus;
+    }
+
+    public Board getFromBoard() {
+        return this.fromBoard;
+    }
+
+    public Board getToBoard() {
+        return this.toBoard;
+    }
+
+    public Move getTransitionMove() {
+        return this.transitionMove;
     }
 
     public MoveStatus getMoveStatus() {
         return this.moveStatus;
     }
-
-    public Board getTransitionBoard() {
-        return this.transitionBoard;
-    }
-
-
 }
