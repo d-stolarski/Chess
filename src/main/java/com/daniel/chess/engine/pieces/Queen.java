51aa3884951b15d5789ecb98cf7adcf65a31c31f
Daniel Stolarski <d.stolarski@o2.pl>
2023-02-03 09:54:22 +0100

    update Queen class

--- src/main/java/com/daniel/chess/engine/pieces/Queen.java
+++ src/main/java/com/daniel/chess/engine/pieces/Queen.java
@@ -5,78 +5,83 @@ import com.daniel.chess.engine.board.Board;
 import com.daniel.chess.engine.board.BoardUtils;
 import com.daniel.chess.engine.board.Move;
 import com.daniel.chess.engine.board.Move.MajorAttackMove;
-import com.daniel.chess.engine.board.Tile;
-import com.google.common.collect.ImmutableList;
+import com.daniel.chess.engine.board.Move.MajorMove;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class Queen extends Piece {
 
-    private final static int[] CANDIDATE_MOVE_VECTOR_COORDINATES = {-9, -8, -7, -1, 1, 7, 8, 9};
+    private final static int[] CANDIDATE_MOVE_COORDINATES = {-9, -8, -7, -1, 1, 7, 8, 9};
 
-    public Queen(final Alliance pieceAlliance,
-                 final int piecePosition) {
-        super(PieceType.QUEEN, piecePosition, pieceAlliance, true);
+    public Queen(final Alliance alliance, final int piecePosition) {
+        super(PieceType.QUEEN, alliance, piecePosition, true);
     }
 
-    public Queen(final Alliance pieceAlliance,
+    public Queen(final Alliance alliance,
                  final int piecePosition,
                  final boolean isFirstMove) {
-        super(PieceType.QUEEN, piecePosition, pieceAlliance, isFirstMove);
+        super(PieceType.QUEEN, alliance, piecePosition, isFirstMove);
     }
 
     @Override
     public Collection<Move> calculateLegalMoves(final Board board) {
-
         final List<Move> legalMoves = new ArrayList<>();
-
-        for (final int candidateCoordinateOffset : CANDIDATE_MOVE_VECTOR_COORDINATES) {
+        for (final int currentCandidateOffset : CANDIDATE_MOVE_COORDINATES) {
             int candidateDestinationCoordinate = this.piecePosition;
-            while (BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
-                if (isFirstColumnExclusion(candidateDestinationCoordinate, candidateCoordinateOffset) ||
-                        isEightColumnExclusion(candidateDestinationCoordinate, candidateCoordinateOffset)) {
+            while (true) {
+                if (isFirstColumnExclusion(currentCandidateOffset, candidateDestinationCoordinate) ||
+                        isEightColumnExclusion(currentCandidateOffset, candidateDestinationCoordinate)) {
                     break;
                 }
-                candidateDestinationCoordinate += candidateCoordinateOffset;
-                if (BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
-                    final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
-                    if (!candidateDestinationTile.isTileOccupied()) {
-                        legalMoves.add(new Move.MajorMove(board, this, candidateDestinationCoordinate));
+                candidateDestinationCoordinate += currentCandidateOffset;
+                if (!BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
+                    break;
+                } else {
+                    final Piece pieceAtDestination = board.getPiece(candidateDestinationCoordinate);
+                    if (pieceAtDestination == null) {
+                        legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
                     } else {
-                        final Piece pieceAtDestination = candidateDestinationTile.getPiece();
-                        final Alliance pieceAlliance = pieceAtDestination.getPieceAlliance();
-                        if (this.pieceAlliance != pieceAlliance) {
-                            legalMoves.add(new MajorAttackMove(board, this, candidateDestinationCoordinate, pieceAtDestination));
+                        final Alliance pieceAtDestinationAllegiance = pieceAtDestination.getPieceAlliance();
+                        if (this.pieceAlliance != pieceAtDestinationAllegiance) {
+                            legalMoves.add(new MajorAttackMove(board, this, candidateDestinationCoordinate,
+                                    pieceAtDestination));
                         }
                         break;
                     }
                 }
             }
         }
-
-        return ImmutableList.copyOf(legalMoves);
+        return Collections.unmodifiableList(legalMoves);
     }
 
     @Override
-    public Queen movePiece(Move move) {
-        return new Queen(move.getMovedPiece().getPieceAlliance(),
-                move.getDestinationCoordinate());
+    public int locationBonus() {
+        return this.pieceAlliance.queenBonus(this.piecePosition);
+    }
+
+    @Override
+    public Queen movePiece(final Move move) {
+        return PieceUtils.INSTANCE.getMovedQueen(move.getMovedPiece().getPieceAlliance(), move.getDestinationCoordinate());
     }
 
     @Override
     public String toString() {
-        return PieceType.QUEEN.toString();
+        return this.pieceType.toString();
     }
 
-    private static boolean isFirstColumnExclusion(final int currentPosition, final int candidateOffset) {
-        return BoardUtils.FIRST_COLUMN[currentPosition] && (candidateOffset == -1 ||
-        candidateOffset == -9 || candidateOffset == 7);
+    private static boolean isFirstColumnExclusion(final int currentPosition,
+                                                  final int candidatePosition) {
+        return BoardUtils.INSTANCE.FIRST_COLUMN.get(candidatePosition) && ((currentPosition == -9)
+                || (currentPosition == -1) || (currentPosition == 7));
     }
 
-    private static boolean isEightColumnExclusion(final int currentPosition, final int candidateOffset) {
-        return BoardUtils.EIGHTH_COLUMN[currentPosition] && (candidateOffset == -7 ||
-                candidateOffset == 1 || candidateOffset == 9);
+    private static boolean isEightColumnExclusion(final int currentPosition,
+                                                  final int candidatePosition) {
+        return BoardUtils.INSTANCE.EIGHTH_COLUMN.get(candidatePosition) && ((currentPosition == -7)
+                || (currentPosition == 1) || (currentPosition == 9));
     }
+
 }
